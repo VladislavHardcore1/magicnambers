c129ce3df15111057399ff47dbead6ba7d54e64d
Vladislav Semenchykov <vladsemka1@gmail.com>
2023-06-22 22:00:39 +0300

    init Projects

---
+++ Assets/Scripts/MagicNumbers.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MagicNumbers : MonoBehaviour
+{
+    private int _min;
+    private int _max;
+    private int _guess;
+    
+    void Start()
+    {
+        _mix = 1;
+        _max = 1000;
+        Debug.log($"Привет! Загадай число от {{_min}} До {_max}");
+        CalculateGuess();
+        Gebug.log("Твое число {")
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
